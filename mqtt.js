1149c1161a8568ad930d3cc2097879af91604170
thanhson1085 <sonnst@sigma-solutions.eu>
2016-09-13 04:01:02 +0000

    restruct function :rabbit:

--- mqtt.js
+++ mqtt.js
@@ -2,7 +2,11 @@ var kmqtt = angular.module('kmqtt', []);
 kmqtt.provider('kmqtt', function() {
     return {
         '$get': function(){
-            return mqtt.connect(url, options);
+            return {
+                connect: function(url, options) {
+                    mqtt.connect(url, options);
+                }
+            };
         }
     };
 });
